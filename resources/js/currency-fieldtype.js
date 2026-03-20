c7a14bd1d85b0210a25936228789998c193e729a
James Pigott <james.pigott@steadfastcollective.com>
2026-03-20 16:00:04 +0000

    Fix

--- resources/js/currency-fieldtype.js
+++ resources/js/currency-fieldtype.js
@@ -1,4 +1,6 @@
 import Fieldtype from './Currency.vue';
 
-// Should be named [snake_case_handle]-fieldtype
-Statamic.$components.register('currency-fieldtype', Fieldtype);
+Statamic.booting(() => {
+    // Should be named [snake_case_handle]-fieldtype
+    Statamic.$components.register('currency-fieldtype', Fieldtype);
+});
